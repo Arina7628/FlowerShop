27ec71cf4548cede65c886fd368515ca60b10ef7
Elektro <elektro_ph_house@mail.ru>
2021-05-30 13:07:26 +0300

    finished

--- src/Helpers/AuthHelper.js
+++ src/Helpers/AuthHelper.js
@@ -20,8 +20,59 @@ function AuthHeader()
   }
 }
 
+function IsManager()
+{
+  let auth = CheckAuth()
+  return auth ? ((localStorage['role'] == 'manager' || localStorage['role'] == 'administrator') ? true : false) : false;
+}
+
+function IsAdministrator()
+{
+  let auth = CheckAuth()
+  return auth ? (localStorage['role'] == 'administrator' ? true : false) : false;
+}
+
+function Relogin()
+{
+  const login = localStorage.getItem('login')
+  const password = localStorage.getItem('password')
+
+  var request = new XMLHttpRequest();
+  request.open("POST", `https://localhost:44350/connect/token`, true);
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  request.addEventListener("load", function ()
+  {
+      if (request.status == 200)
+      {
+          let data = JSON.parse(request.response);
+          let token = data['access_token']
+          console.log(data)
+          localStorage.setItem('token', token)
+          window.location.href = '/';
+      }
+      else
+      {
+        delete localStorage['token']
+        delete localStorage['role']
+        delete localStorage['login']
+        delete localStorage['password'] 
+      }
+  });
+
+  request.send(
+      "grant_type=password" +
+      "&client_id=quickapp_spa" +
+      "&scope=quickapp_api" +
+      "&client_secret=not_used" +
+      "&username=" + login +
+      "&password=" + password);
+}
+
 module.exports =
 {
     CheckAuth: CheckAuth,
-    AuthHeader: AuthHeader
+    AuthHeader: AuthHeader,
+    IsManager: IsManager,
+    IsAdministrator: IsAdministrator,
+    Relogin: Relogin
 };
--- src/Pages/MyAccount.js
+++ src/Pages/MyAccount.js
@@ -1,11 +1,137 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import { Card, Form, Container, CardDeck, Button } from "react-bootstrap";
+import AuthHelper from '../Helpers/AuthHelper';
+import Spinner from '../Components/Spinner';
 
-export default class MyAccount extends Component {
+export default class Myaccount extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isLoaded: false, name: '', email: '', city: '', address: '', phoneNumber: ''}
+
+        this.nameChange = this.nameChange.bind(this);
+        this.emailChange = this.emailChange.bind(this);
+        this.cityChange = this.cityChange.bind(this);
+        this.addressChange = this.addressChange.bind(this);
+        this.phoneChange = this.phoneChange.bind(this);
+      }
+
+    componentDidMount() {
+        this.renderCart();
+      }
+
+    nameChange(event)
+    {
+        this.setState({name: event.target.value});
+    }
+
+    emailChange(event)
+    {
+        this.setState({email: event.target.value});
+    }
+
+    cityChange(event)
+    {
+        this.setState({city: event.target.value});
+    }
+
+    addressChange(event)
+    {
+        this.setState({address: event.target.value});
+    }
+
+    phoneChange(event)
+    {
+        this.setState({phoneNumber: event.target.value});
+    }
+
+      renderCart = async() => {
+        try {
+          const res = await axios.get('https://localhost:44350/api/ShoppingCart', { headers: AuthHelper.AuthHeader() })
+          const account = res.data;
+          this.setState({
+            isLoaded: true,
+            name: account.customer.name,
+            email: account.customer.email,
+            city: account.customer.city,
+            address: account.customer.address,
+            phoneNumber: account.customer.phoneNumber
+          })
+
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
     render() {
+        
+        if (!this.state.isLoaded)
+            return (<Spinner />)
+        
         return (
             <div>
-                MyAccount
+                <div>Мои данные</div>
+                <div>Имя: {this.state.name}</div>
+                <div>Email: {this.state.email}</div>
+                <div>Адрес доставки: {this.state.address ?? "отсутствует"}</div>
+                <div>Родной город: {this.state.city ?? "отсутствует"}</div>
+                <div>Телефон: {this.state.phoneNumber ?? "отсутствует"}</div>
+                <div>-------------------------------------------------------------------</div>
+                <div>-------------------------------------------------------------------</div>
+                <div>----------------------Изменение данных----------------------</div>
+                <div>-------------------------------------------------------------------</div>
+                    <Form>
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" value={this.state.name} onChange={this.nameChange}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="email">
+                            <Form.Label>email</Form.Label>
+                            <Form.Control type="text" placeholder="Enter email" value={this.state.email} onChange={this.emailChange}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="address">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type="text" placeholder="Enter address" value={this.state.address} onChange={this.addressChange}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="city">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="Enter city" value={this.state.city} onChange={this.cityChange}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="phoneNumber">
+                            <Form.Label>Phone</Form.Label>
+                            <Form.Control type="text" placeholder="Enter phone number" value={this.state.phoneNumber} onChange={this.phoneChange}/>
+                        </Form.Group>
+
+                        <Button variant="primary" onClick={() => SaveChanges(this.state.name, this.state.email, 
+                            this.state.address, this.state.city, this.state.phoneNumber)}>Подтвердить изменения</Button>
+                    </Form>
             </div>
         )
     }
+}
+
+async function SaveChanges(name, email, address, city, phoneNumber)
+{
+    let body = 
+    {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        address: address,
+        city: city
+      }
+
+    try
+    {
+        await axios.put(`https://localhost:44350/api/ShoppingCart/MyInfo`, body, { headers: AuthHelper.AuthHeader() })
+    }
+    catch
+    {
+    }
+
+    window.location.reload();
 }
